d69e6f1fc47058a5a9591f5a1ec4ae760bf8f812
Rakesh-Marujodu <rakeshmarujodu2@gmail.com>
2023-11-08 12:23:01 +0000

    First Commit

---
+++ app/src/main/java/uk/ac/tees/w9641526/amazechat/SignUp.kt
@@ -0,0 +1,11 @@
+package uk.ac.tees.w9641526.amazechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
