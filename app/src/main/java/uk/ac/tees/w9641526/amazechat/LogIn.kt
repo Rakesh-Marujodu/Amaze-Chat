5007c86983ab91e71b394132bd57c4b74b0dc7c1
Rakesh-Marujodu <rakeshmarujodu2@gmail.com>
2023-11-16 05:25:31 +0000

    Second Commit: Firebase Authentication setup for Login and Signup

--- app/src/main/java/uk/ac/tees/w9641526/amazechat/LogIn.kt
+++ app/src/main/java/uk/ac/tees/w9641526/amazechat/LogIn.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LogIn : AppCompatActivity() {
 
@@ -14,10 +16,15 @@ class LogIn : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -29,6 +36,38 @@ class LogIn : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password);
+        }
 
     }
+
+    private fun login(email: String, password: String){
+        // login for logging user
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for logging in user
+
+                    val intent = Intent(this@LogIn, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@LogIn, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+
+
+
+
+
+
+
 }
