a590aa46dca500fd890f6512f17196b900fd8a11
Rakesh-Marujodu <w9641526@live.tees.ac.uk>
2023-11-30 15:22:14 +0000

    Fifth Commit: Chat Page

--- app/src/main/java/uk/ac/tees/w9641526/amazechat/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/w9641526/amazechat/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -16,6 +17,7 @@ import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
@@ -24,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         mAuth = FirebaseAuth.getInstance()
         mDbRef = FirebaseDatabase.getInstance().getReference()
@@ -48,8 +52,6 @@ class MainActivity : AppCompatActivity() {
                         userList.add(currentUser!!)
                     }
 
-
-
                 }
 
                 adapter.notifyDataSetChanged()
@@ -70,6 +72,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.logout){
+            //write the logic for logout
             mAuth.signOut()
             val intent = Intent(this@MainActivity,LogIn::class.java)
             finish()
